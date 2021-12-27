b46eea6d91555e1d665d37cbe03fe0cfb8805652
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-27 17:48:52 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes_in_Csharp
+{
+    public class Calculator
+    {
+        [ObsoleteAttribute ("Use The Add List Method that takes many integers")] //Attribute 
+        public static int Add(int FirNumber , int SecNumber)
+        {
+            return FirNumber + SecNumber;
+        }
+
+        // In this case we can't create a many functions with many Parameters that takes a lot of time .
+
+        public static int Add(List<int> Numbers) // Add a List of infinite Numbers inside Function Add();
+        {
+            int Sum = 0;
+
+            foreach (int number in Numbers)
+                Sum += number;
+
+            return Sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Attributes_in_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Result = Calculator.Add(12, 45); // Correcte but Not useful for Calculate sum of Many numbers just tow .
+
+            var Result2 = Calculator.Add(new List<int>() { 2, 45, 16, 35 });
+
+            Console.WriteLine(Result + "\n"+ Result2);
+        }
+    }
+}
